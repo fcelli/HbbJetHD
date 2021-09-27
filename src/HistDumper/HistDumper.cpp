41f81e318b92b885251bd06a4f289b5ccffd4a0f
Federico Celli <federico.celli@physics.ox.ac.uk>
2021-09-27 20:23:35 +0100

    Find associated yield

--- src/HistDumper/HistDumper.cpp
+++ src/HistDumper/HistDumper.cpp
@@ -4,8 +4,8 @@ HistDumper::HistDumper(const RooWorkspace &ws, const TString &region, const TStr
     
     // Loop over pdfs
     RooArgSet all_pdfs = (RooArgSet)ws.allPdfs();
-    TIterator* pdf_iter = all_pdfs.createIterator();
-    RooAbsPdf* pdfi;
+    TIterator *pdf_iter = all_pdfs.createIterator();
+    RooAbsPdf *pdfi;
 	while(pdfi=(RooAbsPdf*)pdf_iter->Next()){
         TString pdfName = (TString)pdfi->GetName();
         if(!pdfName.BeginsWith("pdf__")) continue;
@@ -25,6 +25,23 @@ HistDumper::HistDumper(const RooWorkspace &ws, const TString &region, const TStr
         for (unsigned int ibin=1; ibin<=h->GetNbinsX(); ibin++) {
             h->SetBinError(ibin, 0);
         }
+
+        // Find associated yield
+        RooArgSet all_fcns = (RooArgSet)ws.allFunctions();
+        TIterator* fcn_iter = all_fcns.createIterator();
+        RooRealVar *yieldi;
+        while((yieldi=(RooRealVar*)fcn_iter->Next())){
+            TString yieldName = (TString)yieldi->GetName();
+            if (!yieldName.BeginsWith("yield__") || !yieldName.Contains(procName) || !yieldName.Contains(region)) continue;
+            else {
+                std::cout << "\tyield name:\t" << yieldi->GetName() << "\tyield value:\t" << yieldi->getVal() << std::endl;
+                break;
+            }
+        }
+        delete fcn_iter;
+
+        // Scale histogram by corresponding yield
+        h->Scale(yieldi->getVal()/h->Integral());
 
         m_histos.push_back(h);
     }
