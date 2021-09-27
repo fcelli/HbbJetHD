6398547b70afff82f3ce6470596fef647a46f34b
Federico Celli <federico.celli@physics.ox.ac.uk>
2021-09-27 18:04:37 +0100

    Improved formatting

--- src/ArgParser.cpp
+++ src/ArgParser.cpp
@@ -5,14 +5,14 @@ ArgParser::ArgParser(int argc, char **argv){
         usage();
         exit(0);
     }
-    m_inputfile = argv[1];
-    m_outputfile = argv[2];
-    m_workspace = argv[3];
-    m_snapshot = argv[4];
-    m_region = argv[5];
-    m_observable = argv[6];
+    m_inputfile     = argv[1];
+    m_outputfile    = argv[2];
+    m_workspace     = argv[3];
+    m_snapshot      = argv[4];
+    m_region        = argv[5];
+    m_observable    = argv[6];
 }
 
 void ArgParser::usage() const {
-    std::cout<<"Usage: ./src/dump_histos input_file output_file workspace snapshot region observable"<<std::endl;
+    std::cout << "Usage: ./src/dump_histos input_file output_file workspace snapshot region observable" << std::endl;
 }
